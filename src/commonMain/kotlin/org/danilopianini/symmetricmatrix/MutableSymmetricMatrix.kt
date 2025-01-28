5d767ac3261acc9800920761c264063d2ba00309
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-01-28 12:16:38 +0100

    docs: add doc for Mutable Symmetric Matrix

--- src/commonMain/kotlin/org/danilopianini/symmetricmatrix/MutableSymmetricMatrix.kt
+++ src/commonMain/kotlin/org/danilopianini/symmetricmatrix/MutableSymmetricMatrix.kt
@@ -1,13 +1,26 @@
 package org.danilopianini.symmetricmatrix
 
+/**
+ * A mutable symmetric matrix:
+ * a matrix where the element at position (i, j) is the same as the element at position (j, i).
+ */
 interface MutableSymmetricMatrix<T> : SymmetricMatrix<T> {
 
+    /**
+     * Set the element at position ([i], [j]) to [value].
+     */
     operator fun set(i: Int, j: Int, value: T)
 
     companion object {
 
+        /**
+         * Create a symmetric matrix of [size] with the same [element].
+         */
         fun <T> fill(size: Int, element: T) = fill(size) { _, _ -> element }
 
+        /**
+         * Create a symmetric matrix of [size] with the same [element].
+         */
         inline fun <T> fill(size: Int, crossinline element:(i: Int, j: Int) -> T): MutableSymmetricMatrix<T> =
             object : AbstractSymmetricMatrix<T>(), MutableSymmetricMatrix<T> {
 
