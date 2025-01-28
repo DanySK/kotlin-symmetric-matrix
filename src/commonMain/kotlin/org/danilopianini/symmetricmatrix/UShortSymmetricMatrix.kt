db331600f852ba67209d065bd9ac2f045cab5883
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-01-28 12:06:07 +0100

    feat: add UShort symmetric matrix

---
+++ src/commonMain/kotlin/org/danilopianini/symmetricmatrix/UShortSymmetricMatrix.kt
@@ -0,0 +1,21 @@
+package org.danilopianini.symmetricmatrix
+
+import kotlin.math.max
+import kotlin.math.min
+
+/**
+ * Application entrypoint.
+ */
+class MutableUShortSymmetricMatrix(override val size: Int) : AbstractSymmetricMatrix<UShort>(), MutableSymmetricMatrix<UShort> {
+
+    @OptIn(ExperimentalUnsignedTypes::class)
+    private val data: UShortArray = UShortArray(size * (size + 1) / 2)
+
+    @OptIn(ExperimentalUnsignedTypes::class)
+    override operator fun get(i: Int, j: Int): UShort = data[indexOf(i, j)]
+
+    @OptIn(ExperimentalUnsignedTypes::class)
+    override operator fun set(i: Int, j: Int, value: UShort) {
+        data[indexOf(i, j)] = value
+    }
+}
