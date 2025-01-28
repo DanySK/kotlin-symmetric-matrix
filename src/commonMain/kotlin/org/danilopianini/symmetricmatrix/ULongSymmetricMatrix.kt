b8d06bfde586937475cc116518b60468f226876f
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-01-28 12:05:42 +0100

    feat: add ULong symmetric matrix

---
+++ src/commonMain/kotlin/org/danilopianini/symmetricmatrix/ULongSymmetricMatrix.kt
@@ -0,0 +1,21 @@
+package org.danilopianini.symmetricmatrix
+
+import kotlin.math.max
+import kotlin.math.min
+
+/**
+ * Application entrypoint.
+ */
+class MutableULongSymmetricMatrix(override val size: Int) : AbstractSymmetricMatrix<ULong>(), MutableSymmetricMatrix<ULong> {
+
+    @OptIn(ExperimentalUnsignedTypes::class)
+    private val data: ULongArray = ULongArray(size * (size + 1) / 2)
+
+    @OptIn(ExperimentalUnsignedTypes::class)
+    override operator fun get(i: Int, j: Int): ULong = data[indexOf(i, j)]
+
+    @OptIn(ExperimentalUnsignedTypes::class)
+    override operator fun set(i: Int, j: Int, value: ULong) {
+        data[indexOf(i, j)] = value
+    }
+}
