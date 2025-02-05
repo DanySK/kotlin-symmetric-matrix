8c054f720af20e405863210ce5d484a678cdb890
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-02-05 16:48:30 +0100

    fix: fix bugged index mapping (#10)

--- src/commonTest/kotlin/org/danilopianini/symmetricmatrix/TestMutableDoubleSymmetricMatrix.kt
+++ src/commonTest/kotlin/org/danilopianini/symmetricmatrix/TestMutableDoubleSymmetricMatrix.kt
@@ -6,21 +6,29 @@ import kotlin.test.assertFailsWith
 
 class TestMutableDoubleSymmetricMatrix {
     @Test
-    fun getReturnsCorrectValue() {
-        val matrix = MutableDoubleSymmetricMatrix(3)
-        matrix[0, 1] = 1.5
-        assertEquals(1.5, matrix[0, 1])
+    fun `create a matrix with a single element`() {
+        val matrix = MutableDoubleSymmetricMatrix(1)
+        assertEquals(matrix.size, 1)
+        matrix[0, 0] = SAMPLE_VALUE
+        assertEquals(SAMPLE_VALUE, matrix[0, 0])
     }
 
     @Test
-    fun setUpdatesValueCorrectly() {
+    fun `get should return the expected value`() {
         val matrix = MutableDoubleSymmetricMatrix(3)
-        matrix[1, 2] = 2.5
-        assertEquals(2.5, matrix[1, 2])
+        matrix[0, 1] = SAMPLE_VALUE
+        assertEquals(SAMPLE_VALUE, matrix[0, 1])
     }
 
     @Test
-    fun getThrowsExceptionForInvalidIndices() {
+    fun `set should update value correctly`() {
+        val matrix = MutableDoubleSymmetricMatrix(3)
+        matrix[1, 2] = SAMPLE_VALUE
+        assertEquals(SAMPLE_VALUE, matrix[1, 2])
+    }
+
+    @Test
+    fun `get throws exception for invalid indices`() {
         val matrix = MutableDoubleSymmetricMatrix(3)
         assertFailsWith<IllegalArgumentException> {
             matrix[3, 0]
@@ -31,27 +39,42 @@ class TestMutableDoubleSymmetricMatrix {
     }
 
     @Test
-    fun setThrowsExceptionForInvalidIndices() {
+    fun `set throws exception for invalid indices`() {
         val matrix = MutableDoubleSymmetricMatrix(3)
         assertFailsWith<IllegalArgumentException> {
-            matrix[3, 0] = 1.0
+            matrix[3, 0] = SAMPLE_VALUE
         }
         assertFailsWith<IllegalArgumentException> {
-            matrix[0, 3] = 1.0
+            matrix[0, 3] = SAMPLE_VALUE
         }
     }
 
     @Test
-    fun getReturnsSymmetricValue() {
+    fun `get should work symmetrically`() {
         val matrix = MutableDoubleSymmetricMatrix(3)
-        matrix[1, 2] = 3.5
-        assertEquals(3.5, matrix[2, 1])
+        matrix[1, 2] = SAMPLE_VALUE
+        assertEquals(SAMPLE_VALUE, matrix[2, 1])
     }
 
     @Test
-    fun setUpdatesSymmetricValueCorrectly() {
+    fun `set should update values symmetrically`() {
         val matrix = MutableDoubleSymmetricMatrix(3)
-        matrix[2, 1] = 4.5
-        assertEquals(4.5, matrix[1, 2])
+        matrix[2, 1] = SAMPLE_VALUE
+        assertEquals(SAMPLE_VALUE, matrix[1, 2])
+    }
+
+    @Test
+    fun `on a 4x4 changing 0-2 does not change 1-1`() {
+        val differentSample = SAMPLE_VALUE + 1
+        val matrix = MutableDoubleSymmetricMatrix(4)
+        matrix[1, 1] = SAMPLE_VALUE
+        assertEquals(SAMPLE_VALUE, matrix[1, 1])
+        matrix[0, 2] = differentSample
+        assertEquals(differentSample, matrix[0, 2])
+        assertEquals(SAMPLE_VALUE, matrix[1, 1])
+    }
+
+    companion object {
+        private const val SAMPLE_VALUE = 5.5
     }
 }
